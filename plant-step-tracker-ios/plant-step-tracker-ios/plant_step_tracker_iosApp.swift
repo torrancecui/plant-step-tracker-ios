eca38318e7a86189b1b02e35479dc772364cd8c3
Torrance Cui <cuitorrance@gmail.com>
2023-05-14 18:50:31 -0400

    init app

---
+++ plant-step-tracker-ios/plant-step-tracker-ios/plant_step_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  plant_step_tracker_iosApp.swift
+//  plant-step-tracker-ios
+//
+//  Created by Torrance Cui on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct plant_step_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
